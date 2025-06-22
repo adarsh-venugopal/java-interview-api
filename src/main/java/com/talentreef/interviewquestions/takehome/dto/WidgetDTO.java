56f0fd1a2654caf55f255cc72ce2422f9d91c3f8
Adarsh Venugopal <adarsh@ggexcel.ai>
2025-06-22 23:21:11 +0530

    Add a not null validation for price

--- src/main/java/com/talentreef/interviewquestions/takehome/dto/WidgetDTO.java
+++ src/main/java/com/talentreef/interviewquestions/takehome/dto/WidgetDTO.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 
@@ -17,8 +18,9 @@ public record WidgetDTO(
     @Size(min = 5, max = 1000, message = "Description must be between 5 and 1000 characters")
     String description,
 
-    @DecimalMin(value = "1.00", inclusive = true, message = "Price must be at least 1.00")
-    @DecimalMax(value = "20000.00", inclusive = true, message = "Price must not exceed 20000.00")
+    @NotNull(message = "Price is required")
+    @DecimalMin(value = "1.00", message = "Price must be at least 1.00")
+    @DecimalMax(value = "20000.00", message = "Price must not exceed 20000.00")
     @Digits(integer = 5, fraction = 2, message = "Price can have only up to 2 decimal places")
     BigDecimal price
     ) {}
